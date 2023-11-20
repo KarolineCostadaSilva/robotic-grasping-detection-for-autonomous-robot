591f26eacd298bcf46cfad972796bd7d42b608b5
Karoline J <124310438+KarolineCostadaSilva@users.noreply.github.com>
2023-11-20 06:32:07 -0300

    add graspings.png

--- network.py
+++ network.py
@@ -151,6 +151,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     summary(model, input_size=(3, 224, 224)) # Imprime um resumo da rede
+    # torch.save(model, 'graspnet_model_complete.pth')
 
 
 
